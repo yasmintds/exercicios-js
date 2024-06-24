23751c952199b4c8f58a882b31439a9a469b0e7c
yasmintds <162698196+yasmintds@users.noreply.github.com>
2024-06-24 08:48:34 -0300

    Add files via upload

---
+++ aula 4/script.js
@@ -0,0 +1,26 @@
+//Fetch Teste 01 Estudando as Promisses
+
+//const req = fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+//console.log(req);
+
+//Fetch Teste 02 Resolvendo a Promisse
+fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//     .then(resp =>  console.log(resp))
+//     .catch{ error => console.log(error)};
+
+
+//Fetch Teste 03 Convertendo a respostas em JSON
+//  fetch ('https://pokeapi.co/api/v2/pokemon/pikachu')
+//  .then(resp => resp.json())
+//  .then(data => console.log(data))
+//  .catch(errorn => console.log(error));
+ 
+//Fetch Teste 04 Convertendo a respostas em JSON
+//  fetch ('https://pokeapi.co/api/v2/pokemon/pikachu')
+//  .then(resp => resp.json())
+//  .then(data => console.log(data))
+//  .catch(error  => console.log(error));
+
+
+ 
+     
